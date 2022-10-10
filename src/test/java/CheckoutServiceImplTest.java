3226d367cd6d10ddc79b79ce729a896ac4d77810
Vigneshkumar Rajasekar <vigneshkumar.rajasekar@gmail.com>
2022-10-10 16:22:56 +0100

    Added more test cases

--- src/test/java/CheckoutServiceImplTest.java
+++ src/test/java/CheckoutServiceImplTest.java
@@ -87,5 +87,39 @@ public class CheckoutServiceImplTest {
         Assert.assertEquals("£36.01",checkoutService.proceedToCheckout(testCase3));
     }
 
+    /**
+     * Positive test case 4 :
+     * "Buying 4 quantities of The Terrible Privacy of Maxwell Sim will cost £44.93"
+     * The Terrible Privacy of Maxwell Sim is published after the year 2000, so the discount of 10% will be
+     * applied for the book and sum costs over £30, so a discount of 5% is applied from the total bill
+     */
+    @Test
+    public void testProceedToCheckoutCase4() throws Exception {
+        JSONArray testCase4 = new JSONArray(new String(Files.readAllBytes(Paths.get(Constant.TEST_CASE4))));
+        when(checkoutServiceMock.proceedToCheckout(testCase4)).thenReturn("£44.93");
+        Assert.assertEquals("£44.93",checkoutService.proceedToCheckout(testCase4));
+    }
 
+    /**
+     * Negative test case 1 :
+     * When the input is an empty object. Handled to return 0.00
+     */
+    @Test
+    public void testProceedToCheckout_emptyObject() throws Exception {
+        JSONArray emptyObj = new JSONArray();
+        when(checkoutServiceMock.proceedToCheckout(emptyObj)).thenReturn("");
+        Assert.assertEquals("£0.00",checkoutService.proceedToCheckout(emptyObj));
+    }
+
+    /**
+     * Negative test case 2 :
+     * When the input is an invalid book.
+     * It should ignore the invalid book and proceed with the rest of the books.
+     */
+    @Test
+    public void testProceedToCheckoutCase5() throws Exception {
+        JSONArray testCase5 = new JSONArray(new String(Files.readAllBytes(Paths.get(Constant.TEST_CASE5))));
+        when(checkoutServiceMock.proceedToCheckout(testCase5)).thenReturn("£24.69");
+        Assert.assertEquals("£24.69",checkoutService.proceedToCheckout(testCase5));
+    }
 }
