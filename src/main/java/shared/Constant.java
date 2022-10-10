d3c26f03ed7a96cb142e5c0ce7a034d81182b684
Vigneshkumar Rajasekar <vigneshkumar.rajasekar@gmail.com>
2022-10-10 14:15:08 +0100

    Added Constant file to hold the constants to share it accross the project

---
+++ src/main/java/shared/Constant.java
@@ -0,0 +1,17 @@
+package shared;
+
+import java.text.DecimalFormat;
+
+/**
+ * Constants used internally by book-store
+ * @Author Vigneshkumar
+ */
+
+public class Constant {
+    public static final String BOOK_REPO = "bookrepo.json";
+    public static final String ORDER_DETAIL = "orderdetail.json";
+    public static final String BOOK_KEY_NAME = "name";
+    public static final String BOOK_KEY_PRICE = "price";
+    public static final String BOOK_KEY_YEAR = "year";
+
+}
