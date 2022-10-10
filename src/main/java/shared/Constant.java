800ad04a30b8cdf2b5cbb45623e3e962e51745d0
Vigneshkumar Rajasekar <vigneshkumar.rajasekar@gmail.com>
2022-10-10 16:20:05 +0100

    Added java docs

--- src/main/java/shared/Constant.java
+++ src/main/java/shared/Constant.java
@@ -4,22 +4,32 @@ import java.text.DecimalFormat;
 
 /**
  * Constants used internally by book-store
- * @Author Vigneshkumar
+ * @author Vigneshkumar
  */
 
 public class Constant {
-    public static final String TOTAL_DISCOUNT_REPO = "";
-    public static final String BOOK_REPO = "bookrepo.json";
-    public static final String ORDER_DETAIL = "orderdetail.json";
-    public static final String TEST_CASE1 = "testcase1.json";
-    public static final String TEST_CASE2 = "testcase2.json";
-    public static final String TEST_CASE3 = "testcase3.json";
-    public static final String TEST_CASE4 = "testcase4.json";
+
     public static final String POUND_CURRENCY_SYMBOL = "£";
-    public static final String BOOK_KEY_NAME = "name";
     public static final String BOOK_KEY_PRICE = "price";
     public static final String BOOK_KEY_YEAR = "year";
-    public static final String BOOK_KEY_QUANTITY = "quantity";
     public static final DecimalFormat dblFormat = new DecimalFormat("0.00");
 
+    /**
+     * BOOK_REPO will hold the bookrepo.json file path, which contains the entire book detaials
+     */
+    public static final String BOOK_REPO = "src/main/resources/bookrepo.json";
+
+    /**
+     * ORDER_DETAIL will hold the orderdetail.json file path. This is used in mail class for testing
+     */
+    public static final String ORDER_DETAIL = "src/main/resources/orderdetail.json";
+
+    /**
+     * Test cases are loaded from the json files
+     */
+    public static final String TEST_CASE1 = "src/test/resources/testcase1.json";
+    public static final String TEST_CASE2 = "src/test/resources/testcase2.json";
+    public static final String TEST_CASE3 = "src/test/resources/testcase3.json";
+    public static final String TEST_CASE4 = "src/test/resources/testcase4.json";
+    public static final String TEST_CASE5 = "src/test/resources/testcase5.json";
 }
