bd59210915ce562ed543cef9b52decc9eb7aac7d
Vigneshkumar Rajasekar <vigneshkumar.rajasekar@gmail.com>
2022-10-10 14:28:48 +0100

    Added more constants to handle the book repo and test cases

--- src/main/java/shared/Constant.java
+++ src/main/java/shared/Constant.java
@@ -8,10 +8,18 @@ import java.text.DecimalFormat;
  */
 
 public class Constant {
+    public static final String TOTAL_DISCOUNT_REPO = "";
     public static final String BOOK_REPO = "bookrepo.json";
     public static final String ORDER_DETAIL = "orderdetail.json";
+    public static final String TEST_CASE1 = "testcase1.json";
+    public static final String TEST_CASE2 = "testcase2.json";
+    public static final String TEST_CASE3 = "testcase3.json";
+    public static final String TEST_CASE4 = "testcase4.json";
+    public static final String POUND_CURRENCY_SYMBOL = "£";
     public static final String BOOK_KEY_NAME = "name";
     public static final String BOOK_KEY_PRICE = "price";
     public static final String BOOK_KEY_YEAR = "year";
+    public static final String BOOK_KEY_QUANTITY = "quantity";
+    public static final DecimalFormat dblFormat = new DecimalFormat("0.00");
 
 }
